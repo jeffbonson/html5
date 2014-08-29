ae8df7b33247e5f50124665e3097ed93c7e98f63
jeffbonson <jeffbonson@gmail.com>
2014-08-29 16:13:32 +0530

    mediaquery

---
+++ sprites.rb
@@ -0,0 +1,27 @@
+
+<style>
+img.home {
+    width: 46px;
+    height: 44px;
+    background: url(img_navsprites.gif) 0 0;
+}
+
+img.next {
+    width: 43px;
+    height: 44px;
+    background: url(img_navsprites.gif) -91px 0;
+}
+
+img.previous {
+    width: 43px;
+    height: 44px;
+    background: url(img_navsprites.gif) -315px 0;
+}
+
+</style>
+</head>
+<body>
+
+<img class="home" ><br><br>
+<img class="next" >
+<img class="previous" >
